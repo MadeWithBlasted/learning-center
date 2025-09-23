5f2a0fd68975a42f4bf15cffbc85c3b38d1dd025
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-23 12:04:01 -0500

    feat(routing): add routes for home and about views with titles and redirection setup.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {Home} from './shared/presentation/views/home/home';
+const about = () => import('./shared/presentation/views/about/about').then(m => m.About);
+const baseTitle = 'ACME Learning Center';
+export const routes: Routes = [
+  { path: '/home', component: Home, title: `Home - ${baseTitle}` },
+  { path: '/about', loadComponent: about, title: `About - ${baseTitle}` },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
