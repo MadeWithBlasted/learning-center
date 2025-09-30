8c8a9b7ab18ee74de99cf52f8b1f74ee4579cedb
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-30 12:37:17 -0500

    feat(learning): enhance learning store with course management and api integration.

--- src/app/learning/application/learning.store.ts
+++ src/app/learning/application/learning.store.ts
@@ -3,7 +3,29 @@ import {Category} from '../domain/model/category.entity';
 import {Course} from '../domain/model/course.entity';
 import {LearningApi} from '../infrastructure/learning-api';
 import {retry} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
+/**
+ * Learning store.
+ *
+ * Learning store,responsible for managing the state of the learning context.
+ * It provides signals for categories, courses, loading state, and error messages.
+ * It also provides methods for adding, updating, deleting categories and courses,
+ * as well as loading categories and courses from the API.
+ *
+ * @example
+ * const categories = learningStore.categories;
+ * const courses = learningStore.courses;
+ * const loading = learningStore.loading;
+ * const error = learningStore.error;
+ *
+ * learningStore.addCategory(category);
+ * learningStore.updateCategory(category);
+ * learningStore.deleteCategory(id);
+ * learningStore.addCourse(course);
+ * learningStore.updateCourse(course);
+ *
+ */
 @Injectable({providedIn: 'root'})
 export class LearningStore {
   // Signals
@@ -20,7 +42,8 @@ export class LearningStore {
   readonly coursesCount = computed(() => this.courses().length);
 
   constructor(private learningApi: LearningApi) {
-    // TODO: Load categories and courses from the API
+    this.loadCategories();
+    this.loadCourses();
   }
 
   getCategoryById(id: number | null | undefined): Signal<Category | undefined> {
@@ -68,6 +91,81 @@ export class LearningStore {
       },
       error: err => {
         this._errorSignal.set(this.formatError(err, 'Failed to delete category'));
+        this._loadingSignal.set(false);
+      }
+    });
+  }
+
+  addCourse(course: Course): void {
+    this._loadingSignal.set(true);
+    this._errorSignal.set(null);
+    this.learningApi.createCourse(course).pipe(retry(2)).subscribe({
+      next: createdCourse => {
+        this._coursesSignal.update(courses => [...courses, createdCourse]);
+        this._loadingSignal.set(false);
+      },
+      error: err => {
+        this._errorSignal.set(this.formatError(err, 'Failed to create course'));
+        this._loadingSignal.set(false);
+      }
+    });
+  }
+
+  updateCourse(course: Course): void {
+    this._loadingSignal.set(true);
+    this._errorSignal.set(null);
+    this.learningApi.updateCourse(course).pipe(retry(2)).subscribe({
+      next: updatedCourse => {
+        this._coursesSignal.update(courses =>
+          courses.map(cat => cat.id === updatedCourse.id ? updatedCourse : cat));
+      },
+      error: err => {
+        this._errorSignal.set(this.formatError(err, 'Failed to update course'));
+        this._loadingSignal.set(false);
+      }
+    });
+  }
+
+  deleteCourse(id: number): void {
+    this._loadingSignal.set(true);
+    this._errorSignal.set(null);
+    this.learningApi.deleteCategory(id).pipe(retry(2)).subscribe({
+      next: () => {
+        this._coursesSignal.update(courses => courses.filter(course => course.id !== id));
+        this._loadingSignal.set(false);
+      },
+      error: err => {
+        this._errorSignal.set(this.formatError(err, 'Failed to delete course'));
+        this._loadingSignal.set(false);
+      }
+    });
+  }
+
+  loadCategories(): void {
+    this._loadingSignal.set(true);
+    this._errorSignal.set(null);
+    this.learningApi.getCategories().pipe(takeUntilDestroyed()).subscribe({
+      next: categories => {
+        this._categoriesSignal.set(categories);
+        this._loadingSignal.set(false);
+      },
+      error: err => {
+        this._errorSignal.set(this.formatError(err, 'Failed to load categories'));
+        this._loadingSignal.set(false);
+      }
+    });
+  }
+
+  loadCourses(): void {
+    this._loadingSignal.set(true);
+    this._errorSignal.set(null);
+    this.learningApi.getCourses().pipe(takeUntilDestroyed()).subscribe({
+      next: courses => {
+        this._coursesSignal.set(courses);
+        this._loadingSignal.set(false);
+      },
+      error: err => {
+        this._errorSignal.set(this.formatError(err, 'Failed to load courses'));
         this._loadingSignal.set(false);
       }
     });
